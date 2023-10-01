d97dd1f5bf27c443cf9c94aeb9f71b369f178361
BBC <bryan.bo.cao@outlook.com>
2023-10-01 19:07:38 -0400

    Correct label_file path

--- intrada/cityscapes.py
+++ intrada/cityscapes.py
@@ -30,7 +30,7 @@ class CityscapesDataSet(BaseDataset):
         # label_name = name.replace("leftImg8bit", "gtFine_labelIds")
         # pdb.set_trace()
         label_name = name.split('/')[1]
-        label_file = '../entropy/color_masks/' + label_name 
+        label_file = '../entropy_rank/color_masks/' + label_name
         # label_file = self.root / 'gtFine' / self.set / label_name
         return img_file, label_file
 
